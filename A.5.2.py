744d5e4f90e64a07aa28823d7bcd0702ee5af5c8
jonortega <jon.ortega.g@gmail.com>
2023-10-15 18:00:46 +0200

    Parar programa si supera el minuto de ejec.

--- A.5.2.py
+++ A.5.2.py
@@ -1,4 +1,5 @@
 import hashlib
+import time
 
 def calcular_sha256(nombre_fichero):
     sha256 = hashlib.sha256()
@@ -10,7 +11,7 @@ def calcular_sha256(nombre_fichero):
             sha256.update(datos)
     return sha256.hexdigest()
 
-def escribir_nuevo_fichero_con_linea_final(nombre_fichero_entrada, nombre_fichero_salida, hex_adecuado):
+def reescribir_fichero(nombre_fichero_entrada, nombre_fichero_salida):
     # Leer el contenido del fichero de entrada
     with open(nombre_fichero_entrada, "r") as entrada:
         contenido = entrada.read()
@@ -18,22 +19,33 @@ def escribir_nuevo_fichero_con_linea_final(nombre_fichero_entrada, nombre_ficher
     with open(nombre_fichero_salida, "w") as salida:
         salida.write(contenido)
 
-        # Agregar la línea adicional
+def escribir_nuevo_fichero_con_linea_final(nombre_fichero_entrada, nombre_fichero_salida, hex_adecuado):
+    reescribir_fichero(nombre_archivo_entrada, nombre_archivo_salida)
+    
+    # Agregar la línea adicional
+    with open(nombre_archivo_salida, "a") as salida:
         linea_adicional = f"{hex_adecuado}\tfe\t100\n"
         salida.write(linea_adicional)
 
 def encontrar_hex_sha256(nombre_fichero_entrada, nombre_fichero_salida):
     valor_hex = "00000000" # Generar un valor hexadecimal inicial
-
+    timeout = time.time() + 60 # Dentro de 60 segundos
     contador = 0
     while True:
+        if time.time() > timeout:
+            reescribir_fichero(nombre_fichero_entrada, nombre_fichero_salida)
+            print("===TIEMPO LIMITE (60s) ALCANZADO===")
+            print(f"Iteraciones calculadas: {contador}")
+            exit(1)
+
         sha256 = calcular_sha256(nombre_fichero_salida)
-        # print(f"SHA-256 Output.txt: {sha256}")
-        if sha256.startswith("0"):
+        print(f"SHA-256 Output.txt: {sha256}")
+
+        if sha256.startswith("00"):
             break
 
         valor_hex = format(int(valor_hex, 16) + contador, "08x")
-        # print(f"Valor HEX_8 calculado: {valor_hex}")
+        print(f"Valor HEX_8 calculado: {valor_hex}")
         escribir_nuevo_fichero_con_linea_final(nombre_fichero_entrada, nombre_fichero_salida, valor_hex)
         contador += 1
 
@@ -48,5 +60,10 @@ if __name__ == "__main__":
     # nombre_archivo_salida = input("Ingrese el nombre del archivo de salida: ")
     nombre_archivo_entrada = "SGSSI-23.CB.02.txt"
     nombre_archivo_salida = "Output.txt"
+    
+    start = time.time()
     crear_fichero_salida(nombre_archivo_entrada, nombre_archivo_salida)
-    print("Fichero de salida creado con éxito.")
+    end = time.time()
+    
+    print(f"Elapsed time: {round(end-start, 6)}s")
+    print("Fichero de salida creado con exito.")
